e5451af5dff9718cceb95166508aac16f34e48e1
RukawadeB <hcq19970108@gmail.com>
2025-06-23 16:09:52 +0100

    optimized menu item component by providing optional icon

--- apps/frontend/components/MenuItem/MenuItem.tsx
+++ apps/frontend/components/MenuItem/MenuItem.tsx
@@ -2,9 +2,10 @@ import { ChevronRight } from 'lucide-react';
 import styles from './MenuItem.module.css';
 import Link from 'next/link';
 
-export const MenuItem = ({ href, children }: { href: string; children: React.ReactNode }) => {
+export const MenuItem = ({ href, children, icon }: { href: string; children: React.ReactNode; icon?: React.ReactNode }) => {
   return (
     <Link href={href} className={styles.menuItem}>
+      {icon && <span className={styles.icon}>{icon}</span>}
       <span className={styles.menuItemText}>{children}</span>
       <ChevronRight className={styles.menuIcon}/>
     </Link>
