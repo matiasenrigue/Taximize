e246da69a708781d088afe40cbb5c921424588bc
FloElmhorst <florian.elmhorst@outlook.de>
2025-06-26 15:06:32 +0100

    added missing geolocation warning

--- apps/frontend/contexts/UserLocationContext/UserLocationContext.tsx
+++ apps/frontend/contexts/UserLocationContext/UserLocationContext.tsx
@@ -13,27 +13,51 @@ const UserLocationContext = createContext<UserLocationContextType|null>(null);
 
 export const UserLocationContextProvider = (props: PropsWithChildren) => {
     const {children} = props;
+
     const [location, setLocation] = useState<Location|null>(null);
+    const [isEnabled, setIsEnabled] = useState<boolean>(true);
 
     useEffect(() => {
-        if (!navigator.geolocation)
+        if (!navigator.geolocation) {
+            setIsEnabled(false);
             return;
+        }
 
         const watchId = navigator.geolocation.watchPosition((position) => {
             const {latitude: lat, longitude: lng} = position.coords;
             setLocation({lat, lng});
+            setIsEnabled(true);
         }, (error) => {
-            console.warn("Error:", error);
+            if (error.code === 1) // PERMISSION_DENIED
+                setIsEnabled(false);
+            else
+                console.warn("Error:", error);
         }, {});
 
         return () => navigator.geolocation.clearWatch(watchId);
     }, []);
 
     return (
-        <UserLocationContext.Provider value={{location}}>
-            {children}
+        <UserLocationContext.Provider value={{
+            location
+        }}>
+            {isEnabled
+                ? children
+                : <LocationWarning/>
+            }
         </UserLocationContext.Provider>
     )
+};
+
+export const LocationWarning = () => {
+    return (
+        <div>
+            <h4>Enable your Location</h4>
+            <p>
+                The app requires access to your location to recommend routes and calculate fares.
+            </p>
+        </div>
+    );
 };
 
 export const useUserLocationContext = () => {
