7e4a0b78d0c645a39ad48eb89fb18db9b8d3e904
FloElmhorst <florian.elmhorst@outlook.de>
2025-06-26 20:26:18 +0100

    added Modals for google-maps routing errors

--- apps/frontend/app/[lang]/map/components/Map.tsx
+++ apps/frontend/app/[lang]/map/components/Map.tsx
@@ -2,9 +2,13 @@
 
 import styles from "./Map.module.css";
 import {AdvancedMarker, Map as GoogleMap, useMap, useMapsLibrary} from "@vis.gl/react-google-maps";
-import { useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {useRide} from "../../../../contexts/RideContext/RideContext";
 import {MAP_CENTER, MAP_ID} from "../../../../constants/constants";
+import {NoRouteFoundModal} from "../../../../components/NoRouteFoundModal";
+import {ModalHandle} from "../../../../components/Modal/Modal";
+import {UnknownLocationModal} from "../../../../components/UnknownLocationModal";
+import {RouteErrorModal} from "../../../../components/RouteErrorModal";
 
 export const Map = (props) => {
     const {className} = props;
@@ -15,6 +19,28 @@ export const Map = (props) => {
     const routesLibrary = useMapsLibrary('routes');
     const [directionsService, setDirectionsService] = useState<google.maps.DirectionsService>();
     const [directionsRenderer, setDirectionsRenderer] = useState<google.maps.DirectionsRenderer>();
+
+    const noRouteModalRef = useRef<ModalHandle>(null!);
+    const unknownLocationModalRef = useRef<ModalHandle>(null!);
+    const routeErrorModalRef = useRef<ModalHandle>(null!);
+
+    const openNoRouteModal = useCallback(() => {
+        if (!noRouteModalRef || typeof noRouteModalRef === "function")
+            return;
+        noRouteModalRef.current.open();
+    }, []);
+
+    const openUnknownLocationModal = useCallback(() => {
+        if (!unknownLocationModalRef || typeof unknownLocationModalRef === "function")
+            return;
+        unknownLocationModalRef.current.open();
+    }, []);
+
+    const openRouteErrorModal = useCallback(() => {
+        if (!routeErrorModalRef || typeof routeErrorModalRef === "function")
+            return;
+        routeErrorModalRef.current.open();
+    }, []);
 
     // initialize directions service and renderer
     useEffect(() => {
@@ -34,32 +60,54 @@ export const Map = (props) => {
                 destination: {placeId: destination.placeId},
                 travelMode: google.maps.TravelMode.DRIVING,
                 provideRouteAlternatives: false
-            })
-            .then(response => {
-                directionsRenderer.setMap(map);
-                directionsRenderer.setDirections(response);
-            })
-            .catch((reason) => {
-                console.warn(reason);
+            }, (result, status) => {
+                switch (status) {
+                    case "OK":
+                        directionsRenderer.setMap(map);
+                        directionsRenderer.setDirections(result);
+                        return;
+                    case "NOT_FOUND":
+                        // at least one of the locations specified in the request's origin, destination, or waypoints could not be geocoded.
+                        openUnknownLocationModal();
+                        return;
+                    case "ZERO_RESULTS":
+                        // no route could be found between the origin and destination.
+                        openNoRouteModal();
+                        return;
+                    case "MAX_ROUTE_LENGTH_EXCEEDED":
+                        // the requested route is too long and cannot be processed
+                    default:
+                        openRouteErrorModal();
+                        console.warn(`Error finding route: ${status}`);
+                }
             });
         return () => directionsRenderer.setMap(null);
-    }, [directionsService, directionsRenderer, origin, destination, map]);
+    }, [directionsService, directionsRenderer, origin, destination, map, openNoRouteModal,
+        openUnknownLocationModal, openRouteErrorModal]);
 
     return (
-        <GoogleMap
-            mapId={MAP_ID}
-            className={className}
-            defaultCenter={MAP_CENTER}
-            defaultZoom={12}
-            gestureHandling={'greedy'}
-            clickableIcons={false}
-            disableDefaultUI={true}>
-            {/*{origin && <AdvancedMarker position={origin}>*/}
-            {/*    <div className={styles.user_marker}/>*/}
-            {/*</AdvancedMarker>}*/}
-            {/*{destination && <AdvancedMarker position={destination}>*/}
-            {/*    <div className={styles.user_marker}/>*/}
-            {/*</AdvancedMarker>}*/}
-        </GoogleMap>
+        <>
+            <NoRouteFoundModal
+                ref={noRouteModalRef}/>
+            <UnknownLocationModal
+                ref={unknownLocationModalRef}/>
+            <RouteErrorModal
+                ref={routeErrorModalRef}/>
+            <GoogleMap
+                mapId={MAP_ID}
+                className={className}
+                defaultCenter={MAP_CENTER}
+                defaultZoom={12}
+                gestureHandling={'greedy'}
+                clickableIcons={false}
+                disableDefaultUI={true}>
+                {/*{origin && <AdvancedMarker position={origin}>*/}
+                {/*    <div className={styles.user_marker}/>*/}
+                {/*</AdvancedMarker>}*/}
+                {/*{destination && <AdvancedMarker position={destination}>*/}
+                {/*    <div className={styles.user_marker}/>*/}
+                {/*</AdvancedMarker>}*/}
+            </GoogleMap>
+        </>
     );
 };
---
+++ apps/frontend/components/UnknownLocationModal.tsx
@@ -0,0 +1,35 @@
+import {ForwardedRef, forwardRef} from "react";
+import {Modal, ModalHandle} from "./Modal/Modal";
+import {useTranslations} from "next-intl";
+import {FlexGroup} from "./FlexGroup/FlexGroup";
+import {Button} from "./Button/Button";
+
+export const UnknownLocationModal = forwardRef((props, ref: ForwardedRef<ModalHandle>) => {
+    const t = useTranslations('UnknownLocationModal');
+
+    function closeModal() {
+        if (!ref || typeof ref === "function")
+            return;
+        ref.current.close();
+    }
+
+    return (
+        <Modal
+            ref={ref}
+            title={t("title")}>
+            <FlexGroup
+                direction={"column"}
+                align={"stretch"}>
+                <p>{t("text")}</p>
+                <FlexGroup
+                    direction={"row"}
+                    align={"stretch"}>
+                    <Button
+                        onClick={closeModal}>
+                        {t("confirmButton")}
+                    </Button>
+                </FlexGroup>
+            </FlexGroup>
+        </Modal>
+    );
+});
