99c7ae06160dd0e0d24fd7281e40c61c22cdb325
RukawadeB <hcq19970108@gmail.com>
2025-07-01 16:44:33 +0100

    updated user hook and statistics hook

---
+++ apps/frontend/hooks/useEarningStatistics.ts
@@ -0,0 +1,62 @@
+import { useState, useCallback } from 'react';
+import  api  from "../lib/axios";
+
+/**
+ * if view is weekly, the breakdown is an array of objects with the following properties:
+ * - label: string; // day of week (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+ * - date: string; // yyyy-mm-dd
+ * - value: number; // earnings
+ * if view is monthly, the breakdown is an array of objects with the following properties:
+ * - label: string; // month number (1-31) 
+ * - date: string; // yyyy-mm
+ * - value: number; // earnings
+ * @returns 
+ */
+interface StatisticsData {
+    totalEarnings: number;
+    view: ViewType;
+    startDate: string;
+    endDate: string;
+    breakdown: Array<{
+        label: string; // day of week or month number
+        date: string; // yyyy-mm-dd
+        value: number; // earnings
+    }>;
+}
+
+type ViewType = 'weekly' | 'monthly';
+interface FetchParams {
+    view: ViewType;
+    startDate?: string;
+    endDate?: string;
+}
+
+
+export const useEarningStatistics = () => {
+    const [earningsStatistics, setEarningsStatistics] = useState<StatisticsData | null>(null);
+    const [isEarningsLoading, setIsEarningsLoading] = useState<boolean>(false);
+    const [earningsError, setEarningsError] = useState<any>(null);
+
+    const fetchEarningsStatistics = useCallback(async (params: FetchParams) => {
+        setIsEarningsLoading(true);
+        setEarningsError(null);
+        try {
+            // e.g. /statistics/earnings?view=weekly&startDate=2025-01-01&endDate=2025-01-31
+            const response = await api.get('/statistics/earnings', { 
+                params: {
+                    view: params.view,
+                    startDate: params.startDate,
+                    endDate: params.endDate,
+                }
+            });
+            setEarningsStatistics(response.data.data); 
+        } catch (err) {
+            setEarningsError(err);
+            console.error("Failed to fetch statistics:", err);
+        } finally {
+            setIsEarningsLoading(false);
+        }
+    }, []); 
+
+    return { earningsStatistics, isEarningsLoading, earningsError, fetchEarningsStatistics };
+};
---
+++ apps/frontend/hooks/useWorktimeStatistics.ts
@@ -0,0 +1,61 @@
+import { useState, useCallback } from 'react';
+import  api  from "../lib/axios";
+
+/**
+ * if view is weekly, the breakdown is an array of objects with the following properties:
+ * - label: string; // day of week (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+ * - date: string; // yyyy-mm-dd
+ * - value: number; // earnings
+ * if view is monthly, the breakdown is an array of objects with the following properties:
+ * - label: string; // month number (1-31) 
+ * - date: string; // yyyy-mm
+ * - value: number; // earnings
+ * @returns 
+ */
+interface WorktimeStatistics {
+    totalWorktime: number;
+    view: ViewType;
+    startDate: string;
+    endDate: string;
+    breakdown: Array<{
+        label: string; // day of week or month number
+        withPassengerTime : number; // worktime with passenger
+        emptyTime: number; // worktime without passenger
+    }>;
+}
+
+type ViewType = 'weekly' | 'monthly';
+interface FetchParams {
+    view: ViewType;
+    startDate?: string;
+    endDate?: string;
+}
+
+export const useWorktimeStatistics = () => {
+    const [worktimeStatistics, setWorktimeStatistics] = useState<WorktimeStatistics | null>(null);
+    const [isWorktimeLoading, setIsWorktimeLoading] = useState<boolean>(false);
+    const [worktimeError, setWorktimeError] = useState<any>(null);
+
+    const fetchWorktimeStatistics = useCallback(async (params: FetchParams) => {
+        setIsWorktimeLoading(true);
+        setWorktimeError(null);
+        try {
+            // e.g. /statistics/worktime?view=weekly&startDate=2025-01-01&endDate=2025-01-31
+            const response = await api.get('/statistics/worktime', { 
+                params: {
+                    view: params.view,
+                    startDate: params.startDate,
+                    endDate: params.endDate,
+                }
+            });
+            setWorktimeStatistics(response.data.data); 
+        } catch (err) {
+            setWorktimeError(err);
+            console.error("Failed to fetch worktime statistics:", err);
+        } finally {
+            setIsWorktimeLoading(false);
+        }
+    }, []); 
+
+    return { worktimeStatistics, isWorktimeLoading, worktimeError, fetchWorktimeStatistics };
+}
