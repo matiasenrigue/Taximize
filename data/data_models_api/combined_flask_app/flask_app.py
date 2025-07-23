d840d0f9ee06c15487f03d6d76516e030a918a2f
Matias Enrigue <matienrigue1@gmail.com>
2025-07-23 21:39:55 +0200

    First Try

--- data/data_models_api/combined_flask_app/flask_app.py
+++ data/data_models_api/combined_flask_app/flask_app.py
@@ -240,4 +240,4 @@ def home():
     return "Combined Trip Scoring + Hotspot Prediction API is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=False)
