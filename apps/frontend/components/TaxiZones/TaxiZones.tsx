9bd165762ffac0de9a0b3595182eacb0dba58b3c
FloElmhorst <florian.elmhorst@outlook.de>
2025-07-23 18:38:48 +0100

    implemented ML taxi zone data

--- apps/frontend/components/TaxiZones/TaxiZones.tsx
+++ apps/frontend/components/TaxiZones/TaxiZones.tsx
@@ -1,10 +1,32 @@
-import React, {useEffect} from "react";
-import zones from "../../assets/taxi_zones.json";
+import React, {useEffect, useState} from "react";
+import zoneDataJson from "../../assets/taxi_zones.json";
 import {Polygon} from "../Polygon/Polygon";
 import {COLOR_PRIMARY} from "../../constants/constants";
 import api from "../../lib/axios";
 
+interface JsonZoneData {
+    locationID: string;
+    zone: string;
+    borough: string;
+    center_lat: number;
+    center_lng: number;
+    geometry: number[][][];
+}
+
+interface BackendZoneData {
+    locationId: number;
+    name: string;
+    count: number;
+}
+
+const zoneData: JsonZoneData[] = zoneDataJson;
+
+interface Zone extends BackendZoneData {
+    geometry: number[][][]
+}
+
 export const TaxiZones = () => {
+    const [zones, setZones] = useState<Zone[]>([]);
 
     useEffect(() => {
         api.get("/hotspots").then((response) => {
@@ -19,7 +41,15 @@ export const TaxiZones = () => {
                 return;
             }
 
-            console.log(data);
+            const zonesWithGeometry = (data.zones as BackendZoneData[]).map((zone) => {
+                const _zone = zoneData.find((_zone) => _zone.locationID === zone.locationId.toString());
+                return {
+                    ...zone,
+                    geometry: _zone?.geometry ?? [],
+                    count: Math.max(0, Math.min(1, zone.count / 30))
+                };
+            })
+            setZones(zonesWithGeometry);
 
         }).catch((error) => {
             console.warn(error);
@@ -35,7 +65,7 @@ export const TaxiZones = () => {
                     strokeOpacity={0}
                     strokeWeight={1}
                     fillColor={COLOR_PRIMARY}
-                    fillOpacity={(zone.random / 100) * .8}
+                    fillOpacity={zone.count * .8}
                     paths={zone.geometry.map((path) => (
                         path.map((vertex) => ({
                             lat: vertex[1],
