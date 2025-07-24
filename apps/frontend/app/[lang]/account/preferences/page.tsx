1f27995e29832f47ae157f5bae0a5db888bf93d1
FloElmhorst <florian.elmhorst@outlook.de>
2025-07-24 10:50:30 +0100

    fixed bugs - data: fixed incorrect folder name - preferences: added useCallback to ensure Options work properly

--- apps/frontend/app/[lang]/account/preferences/page.tsx
+++ apps/frontend/app/[lang]/account/preferences/page.tsx
@@ -1,28 +1,28 @@
 "use client"
 
-import React from "react";
+import React, {useCallback} from "react";
 import styles from "./page.module.css";
 import { useTranslations } from "next-intl";
 import { Select, Option } from "../../../../components/Select/Select";
 import { Switch } from "../../../../components/Switch/Switch";
 import { useRouter, usePathname } from "next/navigation";
 import { useLocale } from "next-intl";
-import { useTheme } from "next-themes";  
+import { useTheme } from "next-themes";
 import BackButton from "../../../../components/BackButton/BackButton";
 import {useShift} from "../../../../contexts/ShiftContext/ShiftContext";
 
 export default function Preferences() {
     const t = useTranslations('preferences');
     const locale = useLocale();
-    const { theme, setTheme } = useTheme();
+    const { setTheme } = useTheme();
     const router = useRouter();
     const pathname = usePathname();
     const {showBreakWarnings, setShowBreakWarnings} = useShift();
 
-    const handleLanguageChange = (language: string) => {
+    const handleLanguageChange = useCallback((language: string) => {
         const newPath = pathname.replace(`/${locale}`, `/${language}`);
         router.push(newPath);
-    }
+    }, [pathname, locale, router]);
 
     return (
         <div className={styles.page}>
@@ -39,14 +39,11 @@ export default function Preferences() {
                         <Select
                             onChange={(value) => setTheme(value as string)}>
                             <Option
-                                value="light"
-                                selected={theme === "light"}>{t('light')}</Option>
+                                value="light">{t('light')}</Option>
                             <Option
-                                value="dark"
-                                selected={theme === "dark"}>{t('dark')}</Option>
+                                value="dark">{t('dark')}</Option>
                             <Option
-                                value="system"
-                                selected={theme === "system"}>{t('system')}</Option>
+                                value="system">{t('system')}</Option>
                         </Select>
                     </div>
                     
